65cabd29d137b30376439e5183b548053e8c787b
Bahar <baharalekberova05gmail.com>
2024-07-05 15:54:37 +0400

    Added logo and navigation links to header,styled header using Tailwind CSS utilities

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins:["Poppins", 'sans-serif']
+      },
+      colors: {
+        'bg-color': '#081b29',
+        'second-bg-color': '#112e42',
+        'text-color': '#ededed',
+        'main-color': '#00abf0'
+      }
+    },
   },
   plugins: [],
 }
